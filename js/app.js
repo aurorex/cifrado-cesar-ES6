327b741d72604f86268e7e42e4bf4e36ae6ac7f3
aurorex <selva_beat@hotmail.com>
2018-01-25 15:57:20 +0100

    agregando funcionalidad para el input

--- js/app.js
+++ js/app.js
@@ -1,44 +1,45 @@
-$(document).ready('load', function() {
-  var words = document.getElementById('input');
-  words.addEventListener('click', function() {
-    console.log(words.value);
-    alert('hola');
-  });
-  var convertNumbers = words.value;
-  console.log(convertNumbers);
 
-  if (!isNaN(convertNumbers) || words === '') {
+var words = document.getElementById('input');    //prompt('introduzca texto');
+  
+console.log(words);
+  
+// var convertNumbers = parseInt(words);
+// console.log(convertNumbers);
+
+words.addEventListener('keyup', function() {
+  console.log(words.value);
+  var wordsValue = words.value;
+  if (wordsValue === '' || ) {
     alert('error: ingrese solo letras!');
   } else {
-    var noIsNumber = words;
+    var noIsNumber = wordsValue;
   }
-  
+
   var count = '';
 
+  var cipher = function(wordX) {
+    var wordToUppercase = wordX.toUpperCase();
+    var arr = wordToUppercase;
+    console.log(arr);
+
+    for (var i = 0;i < arr.length;i++) {
+      var b = arr.charCodeAt(i) ;
+    
+      var c = (b - 65 + 33) % 26 + 65;
+      var d = String.fromCharCode(c);
+
+      var sum = count += d;
+      var containerWords = document.getElementById('container-words');
+      var newElement = document.createElement('p');
+      newElement.appendChild(sum.value);
+      containerWords.appendChild(newElement);
+    }
+
+    return containerWords;
+  };
+  document.write(cipher(noIsNumber));
+
+    
 });
 
 
-
-
-
-
-
-
-
-// var cipher = function(wordX) {
-//   var wordToUppercase = wordX.toUpperCase();
-//   var arr = wordToUppercase;
-//   console.log(arr);
-
-//   for (var i = 0;i < arr.length;i++) {
-//     var b = arr.charCodeAt(i) ;
-    
-//     var c = (b - 65 + 33) % 26 + 65;
-//     var d = String.fromCharCode(c);
-
-//     var sum = count += d;
-//   }
-
-//   return sum;
-// }
-// document.write(cipher(noIsNumber));
