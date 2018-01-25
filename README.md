d1608d939884fdb83a386ab4874b911c324dab9b
aurorex <selva_beat@hotmail.com>
2018-01-25 19:36:16 +0100

    modificando funcionalidad

---
+++ README.md
@@ -0,0 +1,5 @@
+# Cifrado César en ES6
+
+### este projecto se encarga de cifrar y descifrar mediante el código ascci las letras que ingresa el usuario en el input.
+
+### las herraminetas tecnologicas que se implemento en este projecto : html5, css3, ECMAScript 6.
--- js/app.js
+++ js/app.js
@@ -1,43 +1,45 @@
+window.addEventListener('load', function() {
+  let words = document.getElementById('input'); 
+  let button = document.getElementById('button1');
+  console.log(words);
 
-var words = document.getElementById('input'); 
-var button = document.getElementById('button1');
-console.log(words);
+  words.addEventListener('keyup', (event) => {
+    console.log(words.value);
+    let wordsValue = words.value;
+    if ((event.keyCode !== 32) && (event.keyCode < 65) || (event.keyCode > 90) && (event.keyCode < 97) || (event.keyCode > 122) || wordsValue.length < 0 || event.keyCode === 32) {
+      alert('error: ingrese solo letras!');
+    } else {
+      wordsValue;
 
-words.addEventListener('keyup', function(event) {
-  console.log(words.value);
-  var wordsValue = words.value;
-  if ((event.keyCode !== 32) && (event.keyCode < 65) || (event.keyCode > 90) && (event.keyCode < 97) || (event.keyCode > 122) || wordsValue.length < 0 || event.keyCode === 32) {
-    alert('error: ingrese solo letras!');
-  } else {
-    wordsValue;
-
-    button.addEventListener('click', function() {
-      var wordsValue = words.value;
-      var cipher = function(wordX) {
-        var wordToUppercase = wordX.toUpperCase();
-        var arr = wordToUppercase;
-        console.log(arr);
-        var count = '';
-      
-        for (var i = 0;i < arr.length;i++) {
-          var b = arr.charCodeAt(i) ;
+      button.addEventListener('click', (event) => {
+        let wordsValue = words.value;
+        let cipher = (wordX) => {
+          let wordToUppercase = wordX.toUpperCase();
+          let arr = wordToUppercase;
+          console.log(arr);
+          let count = '';
         
-          var c = (b - 65 + 33) % 26 + 65;
-          var d = String.fromCharCode(c);
-      
-          var sum = count += d;
-          console.log(sum);
-          var newElement = document.createElement('p');
-          newElement.innerHTML = sum;
-          var containerWords = document.getElementById('container-words');
-          containerWords.appendChild(newElement);
-          console.log(containerWords);
-        }
-        return sum;
-      };
-      document.write(cipher(wordsValue));
-      
-    });
-  }
+          for (var i = 0;i < arr.length;i++) {
+            let b = arr.charCodeAt(i) ;
+          
+            let c = (b - 65 + 33) % 26 + 65;
+            let d = String.fromCharCode(c);
+        
+            let sum = count += d;
+            console.log(sum);
+            let newElement = document.createElement('p');
+            newElement.innerHTML = sum;
+            let containerWords = document.getElementById('container-words');
+            containerWords.appendChild(newElement);
+            console.log(containerWords);
+          }
+          return sum;
+        };
+        document.write(cipher(wordsValue));
+      });
+    }
+  });
+
 });
+
 
