9eea9b3f804377078f72e92a6b24e6f61dd5cf34
aurorex <selva_beat@hotmail.com>
2018-01-25 17:16:08 +0100

    agregando funcionalidad para el button

--- js/app.js
+++ js/app.js
@@ -1,45 +1,43 @@
 
-var words = document.getElementById('input');    //prompt('introduzca texto');
-  
+var words = document.getElementById('input'); 
+var button = document.getElementById('button');
 console.log(words);
-  
-// var convertNumbers = parseInt(words);
-// console.log(convertNumbers);
 
-words.addEventListener('keyup', function() {
+words.addEventListener('keyup', function(event) {
   console.log(words.value);
   var wordsValue = words.value;
-  if (wordsValue === '' || ) {
+  if ((event.keyCode !== 32) && (event.keyCode < 65) || (event.keyCode > 90) && (event.keyCode < 97) || (event.keyCode > 122) || wordsValue === '') {
     alert('error: ingrese solo letras!');
   } else {
-    var noIsNumber = wordsValue;
+    wordsValue;
+
+    button.addEventListener('click', function() {
+      var wordsValue = words.value;
+      var cipher = function(wordX) {
+        var wordToUppercase = wordX.toUpperCase();
+        var arr = wordToUppercase;
+        console.log(arr);
+        var count = '';
+      
+        for (var i = 0;i < arr.length;i++) {
+          var b = arr.charCodeAt(i) ;
+        
+          var c = (b - 65 + 33) % 26 + 65;
+          var d = String.fromCharCode(c);
+      
+          var sum = count += d;
+          console.log(sum);
+          var newElement = document.createElement('p');
+          newElement.innerHTML = sum;
+          var containerWords = document.getElementById('container-words');
+          containerWords.appendChild(newElement);
+          console.log(containerWords);
+        }
+        return sum;
+      };
+      document.write(cipher(wordsValue));
+      
+    });
   }
-
-  var count = '';
-
-  var cipher = function(wordX) {
-    var wordToUppercase = wordX.toUpperCase();
-    var arr = wordToUppercase;
-    console.log(arr);
-
-    for (var i = 0;i < arr.length;i++) {
-      var b = arr.charCodeAt(i) ;
-    
-      var c = (b - 65 + 33) % 26 + 65;
-      var d = String.fromCharCode(c);
-
-      var sum = count += d;
-      var containerWords = document.getElementById('container-words');
-      var newElement = document.createElement('p');
-      newElement.appendChild(sum.value);
-      containerWords.appendChild(newElement);
-    }
-
-    return containerWords;
-  };
-  document.write(cipher(noIsNumber));
-
-    
 });
-
 
